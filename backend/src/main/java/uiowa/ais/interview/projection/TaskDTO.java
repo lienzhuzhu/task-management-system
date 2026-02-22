b62ede20bc942403b66498c63fc59b89b05f3fa8
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2026-02-22 01:02:07 -0600

    add NotNull annotation

--- backend/src/main/java/uiowa/ais/interview/projection/TaskDTO.java
+++ backend/src/main/java/uiowa/ais/interview/projection/TaskDTO.java
@@ -1,6 +1,7 @@
 package uiowa.ais.interview.task;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import uiowa.ais.interview.entity.User;
 import uiowa.ais.interview.entity.Task;
